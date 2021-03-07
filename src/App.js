5fc7378ff52c2d69c02d7deb545eef737e94a797
ArielMejia <arielgomezcr01@gmail.com>
2021-03-07 15:06:53 -0600

    Instalacion de react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 //import logo from './logo.svg';
 import './App.css';
 import BrandHeader from './components/BrandHeader/BrandHeader';
-import Cards from './components/Cards/Cards';
-import CardHolder from './components/Cards/CardHolder';
-import Login from './components/Login/Login';
+import Login from './components/Pages/Login';
+//import Cards from './components/Cards/Cards';
+//import CardHolder from './components/Cards/CardHolder';
+
 
 
 function App() {
   return (
     <section>
       <BrandHeader></BrandHeader>
-      <section>
-        <Login></Login>
-      </section>
-      <CardHolder>
-        <Cards></Cards>
-        <Cards></Cards>
-        <Cards></Cards>
-        <Cards></Cards>
-      </CardHolder>
+      <Login></Login>
+      
     </section>
   );
 }
 
 export default App;
+
+/*
+<CardHolder>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+        <Cards></Cards>
+      </CardHolder>
+*/
---
+++ src/components/Pages/Login.js
@@ -0,0 +1,10 @@
+import Login from '../Login/Login';
+function Login(){
+    return(
+        <section>
+             <Login></Login>
+        </section>
+    )
+}
+
+export default Login;
